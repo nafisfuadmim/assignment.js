5983799df9f494620c88214e952c113c80e3bb5e
Nafis Fuad Mim <nafisnfm@.com>
2021-01-22 03:42:59 +0600

    add

---
+++ assignment.js
@@ -0,0 +1,14 @@
+function kilometerToMeter(meter) {
+  var meter = 20000;
+  var kilometerToMeter = (meter/1000);
+  return kilometerToMeter;
+  
+}
+  var kilometers = "This is kilomters"
+var kilometer =(kilometerToMeter(kilometers));
+console.log(kilometer);
+
+
+
+
+gff
