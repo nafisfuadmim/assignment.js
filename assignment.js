13f95a81f245dc80c92d44838d749e3512d6ce48
Nafis Fuad Mim <nafisnfm@.com>
2021-01-22 19:44:04 +0600

    add

--- assignment.js
+++ assignment.js
@@ -2,7 +2,7 @@ function kilometerToMeter(meter) {
   var meter = 20000;
   var kilometerToMeter = (meter/1000);
   return kilometerToMeter;
-  
+
 }
   var kilometers = "This is kilomters"
 var kilometer =(kilometerToMeter(kilometers));
@@ -11,4 +11,31 @@ console.log(kilometer);
 
 
 
-gff
+function budgetCalculator(watch, phone, laptop) {
+  var result = watch + phone + laptop;
+  return result;
+
+}
+
+var result = 50 + 100 + 500;
+var total = result;
+console.log(result);
+
+
+
+
+function megaFriend(friend)
+{
+  var array1 = friend.match(/\w[a-z]{0,}/gi);
+  var result = array1[0];
+
+  for(var x = 1 ; x < array1.length ; x++)
+  {
+    if(result.length < array1[x].length)
+    {
+    result = array1[x];
+    } 
+  }
+  return result;
+}
+console.log(megaFriend('nafis, rakib, tuhin, afridi, ashik, shakib, tahmid, tain ,ontor, mustafizur'));
